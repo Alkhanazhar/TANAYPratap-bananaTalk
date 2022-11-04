acee6eda9b9f5371e70ccbc09bb6521e4a05c01f
Azhar Khan <107482530+Alkhanazhar@users.noreply.github.com>
2022-11-05 04:36:10 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const btnTranslate=document.querySelector("#btn-translate");
+const txtarea=document.querySelector("#txt-area");
+
+btnTranslate.addEventListener("click",function clickByHandler(){
+    // console.log("i clicked");
+    console.log("input"+txtarea.value);
+})
